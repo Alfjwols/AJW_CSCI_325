de4a53cd6255d9c87ce0fae260e08ee5d3290b2a
Alfonso Wolski <alfjwols@ut.utm.edu>
2025-02-07 23:01:30 -0600

    Implemented full testing in main.cpp

--- main.cpp
+++ main.cpp
@@ -14,36 +14,79 @@ using namespace std;
 
 void DisplayVector(Vector vec);
 
+void AddElementsToVector(Vector& vec, int num);
+
 int main() {
   
   Vector vec1, vec2, vec3;
+  int num;
 
-  // testing initializing using reserve
-  vec1.reserve(1);
-  DisplayVector(vec1);
+  cout << "How many elements should vec1 have? ";
+  cin >> num;
+  AddElementsToVector(vec1,num); // demenstrating pushback & []
 
-  // testing pushback
-  vec1.push_back(1);
-  DisplayVector(vec1);
+  cout << "How many elements should vec 2 have? ";
+  cin >> num;
+  AddElementsToVector(vec2,num); // demonstrating pushback & []
+
   
-  vec1.push_back(2);                                                                           
-  DisplayVector(vec1);
-  vec1.push_back(3);                                                                           
-  DisplayVector(vec1);
-  vec1.push_back(4);                                                                           
-  DisplayVector(vec1);
-  vec1.push_back(6);                                                                           
-  DisplayVector(vec1);
-  vec1.push_back(5);
+  cout << "Printing stars equal to number of elements in each: ";
+  for(int i = 0; i < vec1.size() + vec2.size(); i++){ // demonstrating using size() in a loop
+    cout << "*";
+  }
+
+  cout << "Vec1: " << endl;
   DisplayVector(vec1);
 
-  // testing operator =
-  vec2 = vec1;
+  cout << endl;
+  
+  cout << "Vec2: " << endl;
   DisplayVector(vec2);
+
+  cout << endl;
+  
+  cout << "assignment vector vec3 = vec1:" << endl;
+  vec3 = vec1; // demonstrating using assignment
+  DisplayVector(vec3); 
+
+  cout << endl;
+
+  cout << "copy vector vec3 = new Vector(vec2)" << endl;
+   
+  vec3 = Vector(vec2); // demonstratin using copy constructor
+  DisplayVector(vec3);
+  
   
   return 0;
 }
 
+
+
+/**
+ * add a bunch of elements 1-n to the vector for testing
+ *
+ * @param Vector& vec the vector to add elements to
+ * @param int num number of element to add
+ * @pre vec.vec_ptr must be initialized
+ * 
+ */
+void AddElementsToVector(Vector& vec, int num){
+  vec.reserve(num);
+  for(int i = 0; i < num; i++){
+    vec.push_back(i);
+    vec[i/2] += 1;
+  }
+}
+
+
+
+/**
+ * Displays breif description of vector and its elements
+ *
+ * @param Vector vec the vector to display
+ * @pre vec.vec_ptr must be initialized 
+ * 
+ */
 void DisplayVector(Vector vec){
 
   cout << "----" << endl;
