19b8cd853a489d79dbcf05b3b506fc1c170376aa
Alfonso Wolski <alfjwols@ut.utm.edu>
2025-04-04 16:41:08 -0500

    Implemented 'Game' class

---
+++ GameLibrary/Game.cpp
@@ -0,0 +1,10 @@
+/**
+ * @file Game.cpp
+ * @author alfonsowolski
+ * @date 2025-04-04
+ * @brief Implementation of the Game class
+ * 
+ * Implementation of the Game class
+ */
+
+// no need for implementation yet
---
+++ GameLibrary/Game.h
@@ -0,0 +1,25 @@
+/**
+ * @file Game.h
+ * @author alfonsowolski
+ * @date 2025-04-04
+ * @brief Definition of the Game object
+ * 
+ * Definition of the Game object
+ */
+
+#INDEF "GAME_H"
+#define "GAME_H"
+
+class Game{
+ public:
+  
+ private:
+  string Title;
+  string Publisher;
+  string Genre;
+  float HoursPlayed;
+  float Price;
+  short Year;
+}
+
+#endef
